2b7ac2a2e5e7d4d8bf0f3cbaa8737ad603c21855
phdljr <phdljr@gmail.com>
2023-03-15 16:33:56 +0900

    add: Logger

--- src/main/java/phdljr/controller/IndexController.java
+++ src/main/java/phdljr/controller/IndexController.java
@@ -1,5 +1,7 @@
 package phdljr.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,8 +9,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
+    private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+
     @GetMapping("/hello")
     public String index1() {
+        log.trace("{} {} 출력", "값1", "값2");
+        log.debug("{} {} 출력", "값1", "값2");
+        log.info("{} {} 출력", "값1", "값2");
+        log.warn("{} {} 출력", "값1", "값2");
+        log.error("{} {} 출력", "값1", "값2");
         return "hello";
     }
 
